bff5c5adc461738fa16df23812d8e9a3ce2520eb
Talanski <brn.talanski@gmail.com>
2015-02-27 15:00:01 -0300

    Segundo commit

---
+++ app/js/app.js
@@ -0,0 +1,159 @@
+(function($, window, document) {
+
+        var totalValue, people, total, totalFloat; //Inicializando as variáveis globais
+            //Ao clicar no botão de Calcular, executa essa função
+            $("#calc").on("click", function(ev){
+                
+                ev.preventDefault(); //Previne o submit do form
+
+                totalValue = $("#totalvalue").val(); //Valor total da conta
+                totalValue = totalValue.replace(",", "."); // Substitui a virgula por ponto para fazer a operação
+                
+                people = $("#people").val(); //Total de pessoas
+
+                total = calcTotal(totalValue, people); //Chama a função calcTotal() e depois convert o retorno em string
+                totalString = total.toString().replace(".",","); //Substitui o ponto por virgula
+
+                $("#total").val(totalString); //Total na tela
+
+                customValueForm(people);
+                $(".show-custom").show();
+            });
+
+            $(".show-custom").on("click",function(ev){
+                ev.preventDefault();
+                var elem = $("#customsplit");
+                                                       
+                elem.css('display','block');
+                
+                offset = elem.offset();
+                offsetTop = offset.top;
+                $(window).scrollTop(offsetTop);
+                
+                
+            });
+
+            //Ao clicar no botão de Recalcular, executa essa função
+            $("#recalc").on("click",function(ev){
+                    ev.preventDefault(); //Previne o submit do form
+                    
+                    var totalRecalc = Number(0), 
+                        totalRestante = Number(0),
+                        totalReal = Number(totalValue),
+                        customPeople = Number(0);
+                        serviceTax = Number(0);
+
+                    $( "#custom input[type='number']" ).each( function() {
+                        var self =  $(this);
+
+                        if ( $(this).prop("disabled") === false ){
+                            
+                            if ( $("#service").is(":checked") ){
+                               
+                                valor = Number( $(this).val() );
+                                valor = valor + ( valor * 0.1 );
+                                self.val(valor);
+
+                            }else{
+                                
+                                valor = Number($(this).val());
+
+                            }
+
+                            totalRecalc = totalRecalc + valor;  
+                            customPeople += 1;
+
+                        }
+                         
+                    });
+
+                    totalRestante = totalReal - totalRecalc;
+
+                    if ( totalRestante >= 0 ){
+                        resto = ( totalRestante / (people - customPeople) );
+
+                        $("#custom input[type='number']").each(function(){
+                            if ( $(this).is(":disabled") ){
+                                $(this).val(resto.toFixed(2));
+                            }
+
+                        });
+                    }else{
+                        alert ("Os valores da divisão não batem");
+                    }
+            });
+
+
+            function calcTotal(totalvalue, people){ //Recebe os dados do form e calcula o valor total por pessoa
+
+                var totalValue = Number(totalvalue),
+                    totalPeople = Number(people),
+                    total = totalvalue/totalPeople;
+                
+                total = total.toFixed(2); // Arredonda para duas casas decimais
+                
+                return total;
+            }
+
+            function customValueForm(people){//Gera o formulário de divisão personalizada da conta
+                
+                var i;
+
+                $("#custom .content").empty();
+
+                for ( i = 0; i < people; i++){ //Cria uma lista de inputs equivalente a cada membro na divisão
+
+                    label = "<label for=\"custom\"" + i + " class=\"text-left col-sm-12 col-xs-12\">Pessoa " + (i+1) +"</label>";
+                    check = "<div class=\"col-sm-1 col-xs-2\"><div class=\"checkbox\"><label><input type=\"checkbox\" data-toggle=\"custom" + i + "\"/></label></div></div>";
+                    input = "<div class=\"col-sm-11 col-xs-10\"><div class=\"input-group \"><div class=\"input-group-addon\">R$</div><input id=\"custom" + i + "\" type=\"number\" class=\"form-control\" value=\""+ total +"\" disabled/></div></div>"; 
+                    row = "<div class=\"form-group\">" + label + check + input + "</div>";
+                    $("#custom .content").append( row );
+                }
+                
+                //Habilita e desabilita os campos de input
+                $("#custom .content input[type='checkbox']").on("click", function(){
+                    var inputId = $(this).data("toggle"),
+                        input = $("#"+inputId+"");
+
+                    if (input.is(":disabled")){
+                        input.prop('disabled',false);
+                    }
+                    else{
+                        input.prop('disabled',true);
+                    }
+                });
+            }
+
+        $("#openmenu").on("click",function(ev){
+            ev.preventDefault();
+
+            var mainMenu =  $("#mainmenu");
+            $("#overlay").css('display','block');
+
+            mainMenu.css('display','block');
+            mainMenu.css('left', '-'+mainMenu.width()+'px' );
+            mainMenu.animate({
+                left: '0px'
+            },450, 'ease-in');
+
+            $("#overlay, #closemenu").bind("click",function(){
+                $("#overlay").css('display','none');
+                $("#mainmenu").css('display','none');
+            })
+        })
+
+        var Calculadora = {};
+
+        Calculadora.MainMenu ={
+            openMenuLink: "",
+            closeMenuLink: "",
+            overlay: "",
+            mainMenu: "",
+
+            set_values: function(){
+                this.openMenuLink = $("#openmenu");
+                this.openMenuLink = $("#openmenu");
+            }
+
+        }        
+}(window.Zepto, window, document));
---
+++ gulpfile.js
@@ -0,0 +1,158 @@
+/**
+ *
+ *  Web Starter Kit
+ *  Copyright 2014 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.gulp
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+
+'use strict';
+
+// Include Gulp & Tools We'll Use
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+
+var $ = require('gulp-load-plugins')(); //Carrega plugins do gulp dinâmicamente
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var pagespeed = require('psi');
+var reload = browserSync.reload;
+
+var version = require("./app/version.json");
+
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+//Task para minificar JS
+gulp.task( 'minifyjs', function(){
+  return gulp.src(['app/js/**/*.js','!app/js/**/*.min.js'])
+  .pipe($.uglify())
+  .pipe($.rename(function(path){
+    path.basename += "-v" + version.v;
+    path.extname = ".min.js";
+  }))
+  .pipe(gulp.dest('build/js'))
+  .pipe(gulp.dest('dist/js'))
+});
+
+//Task para compilar CSS e minificar CSS
+gulp.task( 'minifycss', function(){
+  return gulp.src(['app/css/**/*.css','!app/css/**/*.min.css'])
+  .pipe($.csso())
+  .pipe($.rename(function(path){
+    path.basename += "-v" + version.v;
+    path.extname = ".min.css";
+  }))
+  .pipe(gulp.dest('build/css'))
+  .pipe(gulp.dest('dist/css'))
+});
+
+//Task para minificar HTML
+gulp.task( 'minifyhtml', function(){
+  var css = "css/app-v" + version.v + ".min.css";
+  var js = "js/app-v" + version.v + ".min.js";
+
+  return gulp.src('app/**/*.html')
+  .pipe($.htmlReplace({
+
+      'replace-css': css,
+      'replace-js': js,
+  }))
+  .pipe($.minifyHtml())
+  .pipe(gulp.dest('dist'))
+});
+
+//Task para otimizar imagens
+gulp.task('images', function () {
+  return gulp.src(['app/*.png'])
+    .pipe($.size({title: 'pre-images'}))
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'post-images'}));
+});
+
+//Task para copiar os arquivos que não passam por nenhum tipo de processamento
+//para a pasta 'dist'
+gulp.task('copy',['copymin'],function(){
+  return gulp.src([
+    'app/*',
+    '!app/*.html',
+    '!app/*.png',
+    '!app/*.appcache'
+  ],{
+      dot: true
+  })
+  .pipe(gulp.dest('dist'))
+});
+
+gulp.task('copymin',function(){
+    return gulp.src([
+    'app/css/*.min.css',
+    'app/js/*.min.js',
+  ],{
+      dot: true
+  })
+  .pipe($.if('*.min.css', gulp.dest('dist/css')))
+  .pipe($.if('*.min.js', gulp.dest('dist/js')))
+});
+
+//Gera o arquivo de manifest atualizado
+gulp.task('manifest',function(){
+  var css = "css/app-v" + version.v + ".min.css";
+  var js = "js/app-v" + version.v + ".min.js";
+  var text = css + "\n" + js;
+  var regex = new RegExp('#replace-begin([^&]+)#replace-end','i');
+
+  return gulp.src(['app/calculadora.appcache'])
+  .pipe($.replace(regex, text))
+  .pipe(gulp.dest('dist'))
+});
+
+// Clean Output Directory
+gulp.task('clean', del.bind(null, ['dist']));
+
+// Watch Files For Changes & Reload
+gulp.task('serve',function () {
+  browserSync({
+    notify: false,
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: ['dist']
+  });
+
+  gulp.watch(['app/**/*.html'], ['minifyhtml',reload]);
+  gulp.watch(['app/css/**/*.{styl,css}'], ['minifycss', reload]);
+  gulp.watch(['app/js/**/*.js'], ['minifyjs', reload]);
+  gulp.watch(['app/img/**/*'], reload);
+});
+
+// Build Production Files, the Default Task
+gulp.task('default', ['clean'], function (cb) {
+  runSequence(['images','minifyjs','minifycss','minifyhtml','copy','manifest'], cb);
+});
